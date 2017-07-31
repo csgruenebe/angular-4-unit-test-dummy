c25c0d01d4379d93ffebc55ab66edda2f112b813
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2017-07-31 20:36:32 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
   browsers: ['Chrome_without_security'],
-  singleRun: false,
+  singleRun: true,
   customLaunchers: {
     Chrome_without_security: {
       base: 'Chrome',
